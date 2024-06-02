6ca47eb4f959123344ee2813f407def00aaeb157
viji <vijayabharanikhaamahalingam@gmail.com>
2024-06-02 21:58:01 +0530

    task02-commit

--- src/component/FullStack.jsx
+++ src/component/FullStack.jsx
@@ -5,7 +5,7 @@ const FullStack = () => {
                 <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
                     <div className="col mb-5">
                         <div className="card h-100">
-                            <img className="card-img-top" src="https://www.guvi.in/blog/wp-content/uploads/2023/07/person-front-computer-working-html.jpg" alt="..." />
+                            <img className="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSypoD-k41dALy4BGizIueyuMZMis-L0DFiNg&s" alt="..." />
                             <div className="card-body p-4">
                                 <div className="text-center">
                                     <h5 className="fw-bolder">MEAN Stack</h5>
@@ -18,7 +18,7 @@ const FullStack = () => {
 
                     <div className="col mb-5">
                         <div className="card h-100">
-                            <img className="card-img-top" src="https://www.guvi.in/blog/wp-content/uploads/2021/05/react-1024x536.jpg" alt="..." />
+                            <img className="card-img-top" src="https://qph.cf2.quoracdn.net/main-qimg-2d78cbdb999329c6617c731c31cee9f2" alt="..." />
 
                             <div className="card-body p-4">
                                 <div className="text-center">
@@ -32,7 +32,7 @@ const FullStack = () => {
 
                     <div className="col mb-5">
                         <div className="card h-100">
-                            <img className="card-img-top" src="https://www.guvi.in/blog/wp-content/uploads/2023/07/html-css-collage-concept-with-person.jpg" alt="..." />
+                            <img className="card-img-top" src="https://www.upgrad.com/_ww3-next/image/?url=https%3A%2F%2Fd2o2utebsixu4k.cloudfront.net%2Ffc_crack_interviews_ds_algorithms_series1_2x_app__16018850034601-935609cc5bd745b585510090f927fa0f.webp&w=3840&q=75" alt="..." />
 
                             <div className="card-body p-4">
                                 <div className="text-center">
